1c8d2513138e88fe05d25d1ba83d8d0b766c6408
annowak <anow@student.agh.edu.pl>
2025-04-06 14:41:37 +0200

    Pierwsza czesc zajec

--- src/main/java/pl/edu/agh/mwo/hellomaven/App.java
+++ src/main/java/pl/edu/agh/mwo/hellomaven/App.java
@@ -1,17 +1,34 @@
 package pl.edu.agh.mwo.hellomaven;
 
+import com.indvd00m.ascii.render.Render;
+import com.indvd00m.ascii.render.api.ICanvas;
+import com.indvd00m.ascii.render.api.IContextBuilder;
+import com.indvd00m.ascii.render.api.IRender;
+import com.indvd00m.ascii.render.elements.PseudoText;
+
 /**
- * Simple main application
+ * Simple main application.
  */
 public class App {
 
+
+    public static final int TEXT_WIDTH = 120;
+    public static final int TEXT_HEIGHT = 20;
+    public static final int RANDOM = 4;
+
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        IRender render = new Render();
+        IContextBuilder builder = render.newBuilder();
+        builder.width(TEXT_WIDTH).height(App.TEXT_HEIGHT);
+        builder.element(new PseudoText("Hello Maven!"));
+        ICanvas canvas = render.render(builder.build());
+        String s = canvas.getText();
+        System.out.println(s);
     }
     
     public int getRandomNumber() {
         // chosen by fair dice roll.
         // guaranteed to be random
-        return 4;
+        return RANDOM;
     }
 }
